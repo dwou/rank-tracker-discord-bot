9e1d94789a9b1a4d21ae71091f2fe9a862d614e0
dwou <111612803+dwou@users.noreply.github.com>
2025-11-22 11:27:15 -0500

    Add cooldown, improve documentation

--- lobby_manager.py
+++ lobby_manager.py
@@ -9,9 +9,10 @@ from basic_functions import debug_print, create_elo_function
 
 class LobbyManager():
   """ A singleton class to manage lobbies. """
-  elo_function = create_elo_function(K=20, diff=100, xtimes=2)
-  keepalive_duration = 30 * 60 # seconds; initial time to keep a lobby alive for
-  refresh_duration = 3 * 60 # seconds; time to keep a lobby alive without activity
+  ELO_FUNCTION = create_elo_function(K=20, diff=100, xtimes=2)
+  KEEPALIVE_DURATION = 30 * 60  # seconds; initial time to keep a lobby alive for
+  REFRESH_DURATION = 3 * 60     # seconds; time to keep a lobby alive without activity
+  COOLDOWN_TIME = 30            # seconds; minimum time between lobby actions (match results)
   lobbies: dict[int, dict] = {} # lobby ID -> {}
   # `lobbies`: key identifier(1,2,3,...) -> dict:
   #   "ID": int,
@@ -24,15 +25,15 @@ class LobbyManager():
   @classmethod
   async def __lobby_autocloser(cls, lobby: dict) -> None:
     """ Periodically check if it's time to close a lobby based on last_interaction."""
-    sleep_duration = cls.keepalive_duration
+    sleep_duration = cls.KEEPALIVE_DURATION
     start_time = time.time()
     while True:
       await asyncio.sleep(sleep_duration)
       now = time.time()
       sleep_duration = \
         max(
-          lobby['last_interaction'] + cls.refresh_duration, # since the last refresh
-          start_time + cls.keepalive_duration, # since lobby creation
+          lobby['last_interaction'] + cls.REFRESH_DURATION, # since the last refresh
+          start_time + cls.KEEPALIVE_DURATION, # since lobby creation
         ) - now
       if sleep_duration < 0:
         debug_print(f"Closing lobby #{lobby['ID']}.")
@@ -46,13 +47,16 @@ class LobbyManager():
         Raise PermissionError if `player` is banned. """
     if player.banned:
       raise PermissionError("Player is banned from ranked.")
-    # Check if they're already in a lobby
+
+    # Check if the player is already in a lobby.
     for lobby in cls.lobbies.values():
       if player in lobby["players"]:
         raise ValueError(f"Player {player.display_name} is already in a lobby.")
-    # Make sure they have a record with this region+platform
+
+    # Make sure the player has a record with this region+platform.
     _ = player.get_record(region, platform)
-    # Find a free lobby ID and create a lobby using it
+
+    # Find a free lobby ID and create a lobby using it.
     for lobby_id in range(1,1000):
       if lobby_id not in cls.lobbies:
         now = time.time()
@@ -70,7 +74,8 @@ class LobbyManager():
         }
         cls.lobbies[lobby_id] = lobby
         debug_print(f'Created lobby #{lobby_id}')
-        # Spawn a task to automatically close the lobby
+
+        # Spawn a task to automatically close the lobby.
         asyncio.create_task(cls.__lobby_autocloser(lobby))
         return lobby
 
@@ -103,25 +108,31 @@ class LobbyManager():
         Raise PermissionError if the player is uninvited or banned. """
     if joiner.banned:
       raise PermissionError("You're banned from ranked.")
-    # Find the host's lobby
+
+    # Find the host's lobby.
     try:
       lobby = cls.find_lobby(host)
     except ValueError as e:
-      raise ValueError("Host not in a lobby.") from e
-    # Check if player is aleady in the lobby
+      raise ValueError(f"Host \"{host.display_name}\" isn't in a lobby.") from e
+
+    # Check if joiner is aleady in the lobby.
     if joiner in lobby['players']:
       raise ValueError("You're already in this lobby.")
-    # Check if player is in another lobby
+
+    # Check if joiner is in another lobby.
     for lobby2 in cls.lobbies.values():
       if joiner in lobby2['players'] and lobby2 != lobby:
-        raise ValueError("You're already in another lobby (use `/leave`).")
-    # Check if lobby is full
+        raise ValueError("You're already in another lobby (use `/leave` to leave).")
+
+    # Check if lobby is full.
     if len(lobby['players']) > 1:
       raise ValueError("Host lobby is full (wait or make a new one).")
-    # Check if player is invited
+
+    # Check if joiner is invited.
     if joiner not in lobby['invited_players']:
       raise PermissionError("You haven't been invited to this lobby (the host has to `/invite` you).")
-    # Add the player and update the lobby
+
+    # Add the joiner to the lobby and update the lobby.
     lobby['players'].add(joiner)
     lobby['records'][joiner] = {'matches_total': 0, 'W': 0, 'L': 0, 'D': 0}
     cls.update_lobby(lobby)
@@ -129,10 +140,10 @@ class LobbyManager():
   @classmethod
   def leave_lobby(cls, player: Player) -> None:
     """ Remove `player` from their lobby.
-        Raise ValueError if the player isn't found. """
+        Raise ValueError if the player isn't found in a lobby. """
     try:
       lobby = cls.find_lobby(player)
-    except:
+    except ValueError:
       raise ValueError("Player not in a lobby.")
     lobby['players'].remove(player)
     del lobby['records'][player]
@@ -143,13 +154,23 @@ class LobbyManager():
   def report_match_result(cls, winner: Player, draw: bool = False) -> str:
     """ Update the W/L/D of both players in the lobby and update their Elos.
         Return a formatted string representing the match results.
-        `winner` can be either player in a draw. """
-    PlayerManager.should_save = True
+        `winner` can be either player in a draw.
+        Raise RuntimeError if the lobby is still on cooldown. """
     lobby = cls.find_lobby(winner)
     region = lobby['region']
     platform = lobby['platform']
+
+    # Check if lobby is still on cooldown.
+    now = time.time()
+    wait_duration = lobby['last_interaction'] + cls.COOLDOWN_TIME - now
+    if wait_duration > 0:
+      raise RuntimeError(
+        f"Can't update lobby: the lobby is on cooldown for {wait_duration:.1f} seconds."
+      )
+
+    # Update the lobby records.
     # Let Player p1 be the winner, and p2 the loser.
-    # Update the lobby results.
+    PlayerManager.should_save = True
     p1,p2 = None,None
     if not draw:
       for player,record in lobby['records'].items():
@@ -168,18 +189,20 @@ class LobbyManager():
           p1 = player
         elif p2 is None:
           p2 = player
-    # Fetch current Elos
+
+    # Fetch current Elos.
     p1_old_elo = p1.get_elo(region, platform)
     p2_old_elo = p2.get_elo(region, platform)
-    # Make sure p1 has the lower elo if there's a draw (for displaying elo change)
+
+    # Make sure p1 has the lower elo if there's a draw (for displaying elo change).
     if draw:
       if p1_old_elo > p2_old_elo:
         p1,p2 = p2,p1
         p1_old_elo = p1.get_elo(region, platform)
         p2_old_elo = p2.get_elo(region, platform)
 
-    # Calculate Elos and update both players
-    result = cls.elo_function(p1_old_elo, p2_old_elo, p1_wins=(0.5 if draw else 1))
+    # Calculate Elos and update both players.
+    result = cls.ELO_FUNCTION(p1_old_elo, p2_old_elo, p1_wins=(0.5 if draw else 1))
     p1_new_elo = p1_old_elo + result['p1_gain']
     p2_new_elo = p2_old_elo + result['p2_gain']
     p1.records[(region, platform)]['elo'] = p1_new_elo
@@ -187,10 +210,10 @@ class LobbyManager():
     p1.records[(region, platform)]['matches_total'] += 1
     p2.records[(region, platform)]['matches_total'] += 1
 
-    # Log the result
+    # Log the result.
     cls.update_match_log(region, platform, p1, p2, draw=draw)
 
-    # Format the return the results string
+    # Format the return the results string.
     result_text = \
       f"[{'D' if draw else 'W'}] {p1.display_name} :green_square: {int(p1_old_elo)} **(+{round(result['p1_gain'])})** ➜ __{int(p1_new_elo)}__"\
       f"\n[{'D' if draw else 'L'}] {p2.display_name} :red_square: {int(p2_old_elo)} **({round(result['p2_gain'])})** ➜ __{int(p2_new_elo)}__"
@@ -209,9 +232,14 @@ class LobbyManager():
     this_dir = os.path.dirname(os.path.abspath(__file__))
     file_path = os.path.join(this_dir, "match_log.csv")
     with open(file_path, 'a+', encoding='u8') as f:
-      f.write( # timestamp,region,platform,winner_ID,loser_ID,{draw "True", "False", "undo"}
+      f.write(
         ','.join([
-          str(int(time.time())), region, platform, winner.ID, loser.ID, 'undo' if undo else str(draw)
+          str(int(time.time())),        # timestamp
+          region,                       # region
+          platform,                     # platform
+          winner.ID,                    # winner ID
+          loser.ID,                     # loser ID
+          'Undo' if undo else str(draw) # draw ("True", "False", "Undo")
         ]) + '\n'
       )
 
